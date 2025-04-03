2a59acac926f71b3a1ca4173bb5434694b1c68b1
Josh <lutz_143@yahoo.com>
2025-04-02 23:33:09 -0400

    updates to navbar dropdown on hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 
-import "./Nav.css";
+import "../css/Nav.css";
 
 function Navbar() {
     const [sidebar, setSidebar] = useState(false);
+    const [aboutDropdown, setAboutDropdown] = useState(false);
+
     const showSidebar = () => setSidebar(!sidebar);
 
     return (
@@ -21,10 +23,18 @@ function Navbar() {
                 <ul className="navbar" onClick={showSidebar}>
                     <li><NavLink to="/">Home</NavLink></li>
 
-                    {/* About Dropdown (Hover to Expand) */}
-                    <li className="dropdown">
+                    {/* About Dropdown (Hover-Based) */}
+                    <li
+                        className="dropdown"
+                        onMouseEnter={() => setAboutDropdown(true)}
+                        onMouseLeave={() => setTimeout(() => setAboutDropdown(false), 300)}
+                    >
                         <NavLink to="/about">About</NavLink>
-                        <ul className="dropdown-menu">
+                        <ul
+                            className={`dropdown-menu ${aboutDropdown ? "show" : ""}`}
+                            onMouseEnter={() => setAboutDropdown(true)}
+                            onMouseLeave={() => setAboutDropdown(false)}
+                        >
                             <li><NavLink to="/about/mission">Mission Statement</NavLink></li>
                             <li><NavLink to="/about/board">Board Members</NavLink></li>
                             <li><NavLink to="/about/involvement">Involvement</NavLink></li>
