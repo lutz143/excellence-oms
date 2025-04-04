3b2d05594de6693fc352a69bc51ffe281cdd1c6d
Josh <lutz_143@yahoo.com>
2025-04-04 12:24:10 -0400

    updates to main navbar styling, added topnavbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 import "../css/Nav.css";
 import logo from "../assets/ExcellenceOMS_1.png";
@@ -12,42 +13,60 @@ function Navbar() {
     const showSidebar = () => setSidebar(!sidebar);
 
     return (
-        <nav id="nav-container">
-            {/* display hamburger menu when media query triggered */}
-            <div className="navbar">
-                <NavLink to="#" className="menu-bars">
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </NavLink>
-            </div>
-            <div className={sidebar ? "mobile active" : "navbar"}>
-                <ul className="navbar" onClick={showSidebar}>
-                    <li><NavLink to="/">Home</NavLink></li>
+        <header>
+            <nav id="nav-container">
+                <div className="topnavbar-container">
+                    <div className="topnavbar">
+                        <div style={{ color: 'white' }}>News</div>
+                        <div style={{ color: 'white' }}>Events</div>
+                        <div style={{ color: 'white' }}>Contact</div>
+                    </div>
+                </div>
+            </nav>
 
-                    {/* About Dropdown (Hover-Based) */}
-                    <li
-                        className="dropdown"
-                        onMouseEnter={() => setAboutDropdown(true)}
-                        onMouseLeave={() => setTimeout(() => setAboutDropdown(false), 300)}
-                    >
-                        <NavLink to="/about">About</NavLink>
-                        <ul
-                            className={`dropdown-menu ${aboutDropdown ? "show" : ""}`}
-                            onMouseEnter={() => setAboutDropdown(true)}
-                            onMouseLeave={() => setAboutDropdown(false)}
-                        >
-                            <li><NavLink to="/about/mission">Mission Statement</NavLink></li>
-                            <li><NavLink to="/about/board">Board Members</NavLink></li>
-                            <li><NavLink to="/about/involvement">Involvement</NavLink></li>
+            <nav className="menu-bars">
+                {/* display hamburger menu when media query triggered */}
+                <div className="navbar">
+                    <NavLink to="#" className="menu-bars">
+                        <FaIcons.FaBars onClick={showSidebar} />
+                    </NavLink>
+                </div>
+            </nav>
+            <nav id="nav-container">
+                <div className="nav-background">
+                    <div className={sidebar ? "mobile active" : "navbar"}>
+                        <ul className="navbar" onClick={showSidebar}>
+                            <li><NavLink to="/">Home</NavLink></li>
+
+                            {/* About Dropdown (Hover-Based) */}
+                            <li
+                                className="dropdown"
+                                onMouseEnter={() => setAboutDropdown(true)}
+                                onMouseLeave={() => setTimeout(() => setAboutDropdown(false), 300)}
+                            >
+                                <NavLink to="/about">About</NavLink>
+                                <ul
+                                    className={`dropdown-menu ${aboutDropdown ? "show" : ""}`}
+                                    onMouseEnter={() => setAboutDropdown(true)}
+                                    onMouseLeave={() => setAboutDropdown(false)}
+                                >
+                                    <li><NavLink to="/about/mission">Mission Statement</NavLink></li>
+                                    <li><NavLink to="/about/board">Board Members</NavLink></li>
+                                    <li><NavLink to="/about/involvement">Involvement</NavLink></li>
+                                </ul>
+                            </li>
+                            <div className="img-container">
+                                <img src={logo} alt="Logo" className="nav-logo" />
+                            </div>
+
+
+                            <li><NavLink to="/grants">Grants</NavLink></li>
+                            <li><NavLink to="/login">Profile</NavLink></li>
                         </ul>
-                    </li>
-
-                    <img src={logo} alt="Logo" className="nav-logo" />
-
-                    <li><NavLink to="/grants">Grants</NavLink></li>
-                    <li><NavLink to="/login">Profile</NavLink></li>
-                </ul>
-            </div>
-        </nav>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 }
 
