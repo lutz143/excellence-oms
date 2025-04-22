c208ad6bfebad3be2754801023fe54a5e0bc0626
Josh <lutz_143@yahoo.com>
2025-04-21 20:51:45 -0400

    footer styling and structure before merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,6 +48,55 @@ function Footer(props) {
                             </ul>
                         </div>
                     </Col>
+                    <Col xs lg="2" className={classes.connectColumn}>
+                        <div className={classes.footerConnectHeader}>
+                            Connect
+                        </div>
+                        <div>
+                            <ul className={classes.iconList}>
+                                <li>
+                                    <div>
+                                        <a href="https://www.linkedin.com/in/joshlutz/" target="_blank" rel="noreferrer" className={classes.socialIcon}>
+                                            <img src="/images/icon-linkedin.png" alt="LinkedIn Logo" />
+                                        </a>
+                                        <span className={classes.iconSpan}>
+                                            <a href="https://www.linkedin.com/in/joshlutz/" target="_blank" rel="noreferrer">LinkedIn</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="https://www.youtube.com" target="_blank" rel="noreferrer" className={classes.socialIcon}>
+                                            <img src="/images/icon-youtube.png" alt="YouTube Logo" />
+                                        </a>
+                                        <span className={classes.iconSpan}>
+                                            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">YouTube</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className={classes.socialIcon}>
+                                            <img src="/images/icon-facebook.png" alt="Facebook Logo" />
+                                        </a>
+                                        <span className={classes.iconSpan}>
+                                            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="https://www.x.com" target="_blank" rel="noreferrer" className={classes.socialIcon}>
+                                            <img src="/images/icon-x-fka-twitter.png" alt="X Logo" />
+                                        </a>
+                                        <span className={classes.iconSpan}>
+                                            <a href="https://www.x.com" target="_blank" rel="noreferrer">X</a>
+                                        </span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </Col>
                 </Row>
                 <Row>
                     <div className={classes.copyrightWrapper}>
