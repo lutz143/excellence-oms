e21c814c3e2a01609f19952156478607e72c0a8b
Josh <lutz_143@yahoo.com>
2025-04-23 21:28:45 -0400

    updates to home page to handle width of banner image and additional section styling as a placeholder

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,34 @@ import { Card, Container, Row, Col } from "react-bootstrap";
 import classes from "../css/Home.module.css";
 
 function Home() {
-    const [sidebar, setSidebar] = useState(false);
-
     return (
-        <section>
-            <Row className={classes.banner}>
-                <div className={classes.bannerOverlay}>
-                    <div className={classes.bannerOverlayImg}>
+        <>
+            <section className={classes.banner}>
+                <div className={classes.bannerImageWrapper}>
+                    <img src="/images/AdobeStock_73382244.jpeg" alt="Banner" className={classes.bannerImage} />
+                    <div className={classes.overlay}></div>
+                    <div className={classes.textContent}>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Consectetur adipiscing elit.</p>
+                        <p>Sed do eiusmod tempor.</p>
                     </div>
                 </div>
-            </Row>
-        </section>
+            </section>
+
+            <section className={classes.section}>
+                <Container>
+                    <h2>About Us</h2>
+                    <p>This is a section below the banner.</p>
+                </Container>
+            </section>
+
+            <section className={classes.sectionAlt}>
+                <Container>
+                    <h2>Our Services</h2>
+                    <p>Here's another section with content.</p>
+                </Container>
+            </section>
+        </>
     );
 }
 
