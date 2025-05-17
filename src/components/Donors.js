06c89bc4acb75d1e99e4a9eb8ce6a6d7cc30928c
Josh <lutz_143@yahoo.com>
2025-05-17 09:14:59 -0400

    initial donors page and styling

--- src/components/Donors.js
+++ src/components/Donors.js
@@ -1,13 +1,53 @@
-import ComingSoon from './ComingSoon';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
-const Grants = () => {
+import classes from "../css/Donors.module.css"
+import bgImage from "../assets/AdobeStock_170237839.jpeg"
+
+const Donors = () => {
     return (
         <>
-            <div>
-                <ComingSoon />
-            </div>
+            <main className={classes.mainContainer} style={{ '--bg-image': `url(${bgImage})` }}>
+                <section style={{ position: 'relative', zIndex: 2 }}>
+                    <div className={classes.donorBanner}>
+                        <Container className={classes.donorWrapper}>
+                            <h2>Support the Future of Oral & Maxillofacial Surgery</h2>
+                        </Container>
+                    </div>
+                    <div className={classes.headline}>
+                        <Container>
+                            <p>At <span className={classes.titleSpan}><span className={classes.title}>Excellence </span>  in OMS</span>, your generosity fuels a powerful mission: to educate, empower, and uplift the next generation of oral and maxillofacial surgeons—while extending compassionate care to underserved communities.</p>
+                        </Container>
+                    </div>
+                    <section style={{ height: '500px' }}>
+                        <Container>
+                            <Row>
+                                <Col xs={12}>
+                                    <Card>
+                                        <Card.Title>Test 1</Card.Title>
+                                        <Card.Body></Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col xs={12}>
+                                    <Card>
+                                        <Card.Title>Test 1</Card.Title>
+                                        <Card.Body></Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col xs={12}>
+                                    <Card>
+                                        <Card.Title>Test 1</Card.Title>
+                                        <Card.Body></Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </section>
+                </section>
+            </main>
         </>
     );
 };
 
-export default Grants;
+export default Donors;
